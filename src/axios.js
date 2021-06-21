b360e49e1480c4ecb20fa0fc31ac2efe28a6be73
Mohammad Sulthan <muhammadsultana@outlook.com>
2021-06-22 00:22:09 +0700

    change baseURL

--- src/axios.js
+++ src/axios.js
@@ -1,8 +1,8 @@
 // axios
 import axios from 'axios'
 
-const baseURL = "http://localhost:8080/"
-// const baseURL = "https://api.certismedia.com/"
+// const baseURL = "http://localhost:8080/"
+const baseURL = "https://api.certismedia.com/"
 
 const instance = axios.create({
   baseURL: baseURL,
